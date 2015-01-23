cea39f8170047a91b2d5aaa20560cc1fee6e081c
binaryDilation <binaryDilation@users.noreply.github.com>
2015-01-23 11:37:58 -0500

    Added splash

--- FFmpeg-Cron-Convert.sh
+++ FFmpeg-Cron-Convert.sh
@@ -686,6 +686,10 @@ green='\033[0;32m'
 cyan='\033[0;36m'
 # Global Bold Variable, uses tput since it's the most compatible, works with non-VT100 terminals (looks up appropriate codes according to TERM)
 bold=`tput bold`
+# Splash
+echo  ' ___  ___        __   ___  __      __       ___  __           __   __             ___  __  ___  ___  __ '
+echo  '|__  |__   |\/| |__) |__  / _` __ |__)  /\   |  /  ` |__| __ /  ` /  \ |\ | \  / |__  |__)  |  |__  |__)'
+echo  '|    |     |  | |    |___ \__>    |__) /~~\  |  \__, |  |    \__, \__/ | \|  \/  |___ |  \  |  |___ |  \'
 # Prints today's date, useful to break up logs
 echo -e "${cyan}${bold}-----------------------------------------------------"
 echo -e "${cyan}${bold}Script Run: $(date)"
